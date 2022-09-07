8e6b48567dc3c17a38b873d0715d46d794f51adc
peafield <peterjohncoles@gmail.com>
2022-09-07 16:51:26 +0100

    Something us up with white spaces

--- main.go
+++ main.go
@@ -22,22 +22,76 @@ func getTextSlice() []string {
 
 func selector(e []string) []string {
 	for i, ch := range e {
-		switch {
-		case ch == "(hex)":
-			e[i-1] = hexAndBin(e[i-1], 16)
-			e = remove(e, i)
-		case ch == "(bin)":
-			e[i-1] = hexAndBin(e[i-1], 2)
-			e = remove(e, i)
-		case ch == "(up)":
-			e[i-1] = up(e[i-1])
-			e = remove(e, i)
-		case ch == "(low)":
-			e[i-1] = low(e[i-1])
-			e = remove(e, i)
-		case ch == "(cap)":
-			e[i-1] = cap(e[i-1])
-			e = remove(e, i)
+		if ch[:1] == "(" {
+			switch {
+			case ch == "(hex)":
+				e[i-1] = hexAndBin(e[i-1], 16)
+				e = remove(e, i)
+			case ch == "(bin)":
+				e[i-1] = hexAndBin(e[i-1], 2)
+				e = remove(e, i)
+			case ch[:2] == "(u":
+				if ch[3:] == "," {
+					a, err := strconv.Atoi(e[i+1][:1])
+					if err != nil {
+						fmt.Println("Error:", err)
+					}
+					for a >= 0 {
+						e[i-a] = up(e[i-a])
+						a--
+					}
+					sum := 2
+					for sum > 0 {
+						e = remove(e, i)
+						sum--
+					}
+
+				} else {
+					e[i-1] = cap(e[i-1])
+					e = remove(e, i)
+				}
+			case ch[:2] == "(l":
+				if ch[4:] == "," {
+					a, err := strconv.Atoi(e[i+1][:1])
+					if err != nil {
+						fmt.Println("Error:", err)
+					}
+					for a >= 0 {
+						e[i-a] = low(e[i-a])
+						a--
+					}
+					sum := 2
+					for sum > 0 {
+						e = remove(e, i)
+						sum--
+					}
+
+				} else {
+					e[i-1] = cap(e[i-1])
+					e = remove(e, i)
+				}
+			case ch[:2] == "(c":
+				if ch[4:] == "," {
+					a, err := strconv.Atoi(e[i+1][:1])
+					if err != nil {
+						fmt.Println("Error:", err)
+					}
+					for a >= 0 {
+						e[i-a] = cap(e[i-a])
+						a--
+					}
+					sum := 2
+					for sum > 0 {
+						e = remove(e, i)
+						sum--
+					}
+
+				} else {
+					e[i-1] = cap(e[i-1])
+					e = remove(e, i)
+				}
+
+			}
 		}
 	}
 	return e
@@ -70,5 +124,6 @@ func remove(slice []string, s int) []string {
 func main() {
 	e := editor{}
 	e.text = getTextSlice()
-	fmt.Println(selector(e.text))
+	str := selector(e.text)
+	fmt.Println(str)
 }
